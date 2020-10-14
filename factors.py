5810fc9ba0ba7b1b535c8e6042f2b375ef6b1b43
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-10-13 21:14:01 -0700

    Add files via upload

---
+++ factors.py
@@ -0,0 +1,23 @@
+n = int(input("Please enter a positive integer:"))
+print("The factors of", n,"are:")
+print(1)
+if n % 2==0:
+    print(2)
+    if n%3 ==0:
+        print(3)
+        if n%4==0:
+            print(4)
+            if n%5==0:
+                print(5)
+            else:
+                if n%6==0:
+                    print(6)
+                    if n%7==0:
+                        print(7)
+                        if n%8==0:
+                            print(8)
+                            if n%9==9:
+                                print(9)
+                                if n%10==10:
+                                    print(10)
+print(n)
